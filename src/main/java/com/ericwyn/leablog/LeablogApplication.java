880a44b433060b0e59aa57b037521100c242453f
Ericwyn <Ericwyn.chen@gmail.com>
2017-12-13 11:07:30 +0800

    Initial commit,add the ConfigGet tool.

---
+++ src/main/java/com/ericwyn/leablog/LeablogApplication.java
@@ -0,0 +1,12 @@
+package com.ericwyn.leablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeablogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeablogApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ericwyn/leablog/api/LeanoteApi.java
@@ -0,0 +1,10 @@
+package com.ericwyn.leablog.api;
+
+/**
+ * Created by Ericwyn on 17-12-13.
+ */
+public class LeanoteApi {
+
+
+
+}
